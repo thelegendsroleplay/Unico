aa981cbf1878d5847b5e652efade467c66d9e751
thelegendsroleplay <bhoslenikhil0@gmail.com>
2026-01-24 21:14:11 +0530

    Show payment receipts in management orders and hide dashboard link

--- unico-voucher-checkout/assets/js/checkout.js
+++ unico-voucher-checkout/assets/js/checkout.js
@@ -14,6 +14,21 @@
 		$box.html(list).addClass('is-visible');
 	}
 
+	function setOtpErrors(errors) {
+		var $box = $('#unico-vc-otp-errors');
+		if (!$box.length) return;
+		if (!errors || !errors.length) {
+			$box.removeClass('is-visible').empty();
+			return;
+		}
+		var list = '<ul>';
+		for (var i = 0; i < errors.length; i++) {
+			list += '<li>' + $('<div/>').text(errors[i]).html() + '</li>';
+		}
+		list += '</ul>';
+		$box.html(list).addClass('is-visible');
+	}
+
 	function getQty() {
 		var qty = parseInt($('#unico-vc-qty').val(), 10);
 		if (!Number.isFinite(qty) || qty < 1) qty = 1;
@@ -66,7 +81,7 @@
 		});
 	}
 
-	function submitOrder() {
+	function createOrder(otpKey) {
 		var $btn = $('#unico-vc-submit');
 		if ($btn.prop('disabled')) return;
 
@@ -94,6 +109,7 @@
 		fd.append('txn_id', txnId);
 		fd.append('confirm', String(confirm));
 		fd.append('bank_key', String(bankKey));
+		fd.append('otp_key', String(otpKey || ''));
 		if (file) fd.append('receipt', file);
 
 		$btn.prop('disabled', true);
@@ -128,13 +144,120 @@
 			});
 	}
 
+	function openOtpModal(email) {
+		$('#unico-vc-otp-email').text(email || '');
+		$('#unico-vc-otp-code').val('');
+		setOtpErrors([]);
+		$('#unico-vc-otp-modal').addClass('is-active').attr('aria-hidden', 'false');
+		setTimeout(function () {
+			$('#unico-vc-otp-code').trigger('focus');
+		}, 100);
+	}
+
+	function closeOtpModal() {
+		$('#unico-vc-otp-modal').removeClass('is-active').attr('aria-hidden', 'true');
+	}
+
+	function requestOtp(otpState) {
+		var buyerName = $('#unico-vc-buyer-name').val() || '';
+		var buyerEmail = $('#unico-vc-buyer-email').val() || '';
+
+		setOtpErrors([]);
+		setErrors([]);
+
+		var payload = {
+			action: 'unico_vc_request_otp',
+			nonce: UnicoVC.otpNonce,
+			buyer_name: buyerName,
+			buyer_email: buyerEmail,
+			otp_key: otpState.key || ''
+		};
+
+		return $.post(UnicoVC.ajaxUrl, payload)
+			.then(function (res) {
+				if (res && res.success && res.data && res.data.otp_key) {
+					otpState.key = res.data.otp_key;
+					openOtpModal(buyerEmail);
+					return true;
+				}
+				var msg = (res && res.data && res.data.message) || 'Unable to send verification code.';
+				setErrors([msg]);
+				return false;
+			})
+			.fail(function (xhr) {
+				var errors = ['Unable to send verification code.'];
+				if (xhr.responseJSON && xhr.responseJSON.data && xhr.responseJSON.data.message) {
+					errors = [xhr.responseJSON.data.message];
+				}
+				setErrors(errors);
+				return false;
+			});
+	}
+
+	function verifyOtp(otpState) {
+		var buyerEmail = $('#unico-vc-buyer-email').val() || '';
+		var otpCode = $('#unico-vc-otp-code').val() || '';
+		setOtpErrors([]);
+
+		return $.post(UnicoVC.ajaxUrl, {
+			action: 'unico_vc_verify_otp',
+			nonce: UnicoVC.otpNonce,
+			buyer_email: buyerEmail,
+			otp_key: otpState.key || '',
+			otp_code: otpCode
+		})
+			.then(function (res) {
+				if (res && res.success) {
+					otpState.verified = true;
+					closeOtpModal();
+					createOrder(otpState.key);
+					return true;
+				}
+				var msg = (res && res.data && res.data.message) || 'Verification failed.';
+				setOtpErrors([msg]);
+				return false;
+			})
+			.fail(function (xhr) {
+				var errors = ['Verification failed.'];
+				if (xhr.responseJSON && xhr.responseJSON.data && xhr.responseJSON.data.message) {
+					errors = [xhr.responseJSON.data.message];
+				}
+				setOtpErrors(errors);
+				return false;
+			});
+	}
+
+	function submitWithOtp(otpState) {
+		if (otpState.verified) {
+			createOrder(otpState.key);
+			return;
+		}
+		requestOtp(otpState);
+	}
+
 	$(function () {
 		if (!$('#unico-vc-checkout').length) return;
+		var otpState = { verified: false, key: null };
 		bindQty();
 		bindUpload();
 		syncQtyBadge();
 		syncTotal();
-		$('#unico-vc-submit').on('click', submitOrder);
+		$('#unico-vc-submit').on('click', function () {
+			submitWithOtp(otpState);
+		});
+		$('#unico-vc-otp-verify').on('click', function () {
+			verifyOtp(otpState);
+		});
+		$('#unico-vc-otp-resend').on('click', function () {
+			requestOtp(otpState);
+		});
+		$('#unico-vc-otp-close').on('click', function () {
+			closeOtpModal();
+		});
+		$('#unico-vc-otp-modal').on('click', function (event) {
+			if (event.target === this) {
+				closeOtpModal();
+			}
+		});
 	});
 })(jQuery);
-
